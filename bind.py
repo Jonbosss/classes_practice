11b571b595161c8ce77ac32a5eb3a03d7638a07e
Jonbosss <jonathanfarhat@gmail.com>
2025-11-20 11:52:18 -0800

    Completed key binding exercise

--- bind.py
+++ bind.py
@@ -4,13 +4,16 @@ root = Tk()
 root.title('Classy')
 root.geometry('400x400')
 
+# Event types <Enter>, <Leave>, <Button-1,2,3>(mouse buttons), <FocusIn>, <FocusOut>, <Return>(Enter Button), <Key> (any keyboard key)
 
-def clicker():
-    myLabel = Label(root, text='YOU DID IT!')
+
+def clicker(event):
+    myLabel = Label(root, text='You clicked this button: ' + event.char)
     myLabel.pack()
 
 
 myButton = Button(root, text='Click Me!', command=clicker)
+myButton.bind('<Key>', clicker)
 myButton.pack(pady=10)
 
 
